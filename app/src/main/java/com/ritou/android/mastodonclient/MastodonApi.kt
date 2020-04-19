3d30e5156b296d3b4e42cd3e4de78c329efacfd6
R.Ito <kawboy442@gmail.com>
2020-04-19 17:23:45 +0900

    feat: タイムライン取得処理をコルーチンで行うようAPI処理を変更

--- app/src/main/java/com/ritou/android/mastodonclient/MastodonApi.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/MastodonApi.kt
@@ -1,12 +1,11 @@
 package com.ritou.android.mastodonclient
 
 import okhttp3.ResponseBody
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface MastodonApi {
 
     @GET ("api/vi/timelines/public")
-    fun fetchPublicTimeline (
-    ): Call<ResponseBody>
+    suspend fun fetchPublicTimeline (
+    ): ResponseBody
 }
