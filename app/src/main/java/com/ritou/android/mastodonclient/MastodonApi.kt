420b94708d9a6244e224e6ac5b6e54291806b110
R.Ito <kawboy442@gmail.com>
2020-04-19 17:15:48 +0900

    feat: MastodonApiにタイムラインの取得リクエスト処理を記述

--- app/src/main/java/com/ritou/android/mastodonclient/MastodonApi.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/MastodonApi.kt
@@ -1,2 +1,12 @@
 package com.ritou.android.mastodonclient
 
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MastodonApi {
+
+    @GET ("api/vi/timelines/public")
+    fun fetchPublicTimeline (
+    ): Call<ResponseBody>
+}
