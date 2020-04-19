07e844ff283cc6bc5d8d3debdb2605939d1960e6
R.Ito <kawboy442@gmail.com>
2020-04-19 14:55:40 +0900

    feat: MainFragmentを改修

--- app/src/main/java/com/ritou/android/mastodonclient/MainFragment.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/MainFragment.kt
@@ -1,4 +1,25 @@
 package com.ritou.android.mastodonclient
 
-class MainFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.ritou.android.mastodonclient.databinding.FragmentMainBinding
+
+class MainFragment: Fragment(R.layout.fragment_main) {
+
+    private var binding: FragmentMainBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = DataBindingUtil.bind(view)
+        binding?.textView?.text = "Hello Fragment"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        binding?.unbind()
+    }
 }
