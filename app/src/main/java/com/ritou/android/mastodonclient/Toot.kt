1227f15fc9a44c2607a6199bdb9aa260bf281e78
R.Ito <kawboy442@gmail.com>
2020-04-19 17:47:22 +0900

    feat: Tootクラスをdata classとし、必要なJSONパース処理設定を記述

--- app/src/main/java/com/ritou/android/mastodonclient/Toot.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/Toot.kt
@@ -1,4 +1,12 @@
 package com.ritou.android.mastodonclient
 
-class Toot {
-}
+import com.squareup.moshi.Json
+
+data class Toot (
+    val id: String,
+    @Json(name = "created_at") val createdAt: String,
+    val sensitive: Boolean,
+    val url: String,
+    val context: String,
+    val account: Account
+)
