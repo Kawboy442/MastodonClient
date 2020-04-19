9af7e01b627cbebcbaaa72ee4688d83f3e49cca2
R.Ito <kawboy442@gmail.com>
2020-04-19 14:39:54 +0900

    feat: MainActivityをDataBindingに合わせて書き換え

--- app/src/main/java/com/ritou/android/mastodonclient/MainActivity.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/MainActivity.kt
@@ -2,15 +2,18 @@ package com.ritou.android.mastodonclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.ritou.android.mastodonclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.textView)
-        textView.text = "Hello, XML Layout!"
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this,
+             R.layout.activity_main
+        )
+        binding.textView.text = "Hello DataBinding!"
     }
 }
