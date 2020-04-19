e186b80902c8d884bf3573ca5c8f95d44d9a7228
R.Ito <kawboy442@gmail.com>
2020-04-19 17:36:07 +0900

    fix: APIのURLが間違っていたのを修正

--- app/src/main/java/com/ritou/android/mastodonclient/MastodonApi.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/MastodonApi.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 
 interface MastodonApi {
 
-    @GET ("api/vi/timelines/public")
+    @GET ("api/v1/timelines/public")
     suspend fun fetchPublicTimeline (
     ): ResponseBody
 }
