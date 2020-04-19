3e6284652a709faffeb4a1706516e8c32289923a
R.Ito <kawboy442@gmail.com>
2020-04-19 15:07:21 +0900

    feat: MainActivityをFragmentを用いて表示させるように改修

--- app/src/main/java/com/ritou/android/mastodonclient/MainActivity.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/MainActivity.kt
@@ -2,18 +2,21 @@ package com.ritou.android.mastodonclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import com.ritou.android.mastodonclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
-            this,
-             R.layout.activity_main
-        )
-        binding.textView.text = "Hello DataBinding!"
-    }
+        if (savedInstanceState == null) {
+            val fragment = MainFragment()
+            supportFragmentManager.beginTransaction()
+                .add(
+                    R.id.fragment_container,
+                    fragment,
+                    MainFragment::class.java.simpleName
+                )
+                .commit()
+        }
+     }
 }
