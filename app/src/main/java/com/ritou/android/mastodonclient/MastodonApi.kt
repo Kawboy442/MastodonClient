1360a8ca7613179d0b738fc4208874b402ecf9ce
R.Ito <kawboy442@gmail.com>
2020-04-19 17:49:34 +0900

    feat: 取得したレスポンスをTootの配列として受け取るように変更

--- app/src/main/java/com/ritou/android/mastodonclient/MastodonApi.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/MastodonApi.kt
@@ -1,11 +1,10 @@
 package com.ritou.android.mastodonclient
 
-import okhttp3.ResponseBody
 import retrofit2.http.GET
 
 interface MastodonApi {
 
     @GET ("api/v1/timelines/public")
     suspend fun fetchPublicTimeline (
-    ): ResponseBody
+    ): List<Toot>
 }
