a508098f4bba436e0797b98a37c8b0db613c4b2e
R.Ito <kawboy442@gmail.com>
2020-04-25 09:46:19 +0900

    feat: APIリクエストにフィルタリング用のクエリパラメータ引数を設定

--- app/src/main/java/com/ritou/android/mastodonclient/data/MastodonApi.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/data/MastodonApi.kt
@@ -2,10 +2,12 @@ package com.ritou.android.mastodonclient.data
 
 import com.ritou.android.mastodonclient.domain.Toot
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MastodonApi {
 
     @GET ("api/v1/timelines/public")
     suspend fun fetchPublicTimeline (
+         @Query("only_media") onlyMedia: Boolean = false
     ): List<Toot>
 }
