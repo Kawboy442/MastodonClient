e39f1bae894d3d5670e511ec2da2cc5fb3a775df
R.Ito <kawboy442@gmail.com>
2020-04-25 09:15:16 +0900

    feat: Mediaのパース処理を記述

--- app/src/main/java/com/ritou/android/mastodonclient/domain/Media.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/domain/Media.kt
@@ -1,4 +1,10 @@
 package com.ritou.android.mastodonclient.domain
 
-class Media {
-}
+import com.squareup.moshi.Json
+
+data class Media(
+    val id: String,
+    val type: String,
+    val url: String,
+    @Json(name = "preview_url") val previewUrl: String
+)
