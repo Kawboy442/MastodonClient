e28f413857a07f612b4735db7acde0d404972376
R.Ito <kawboy442@gmail.com>
2020-04-26 22:47:46 +0900

    feat: LiveDataの値を監視する設定を追加

--- app/src/main/java/com/ritou/android/mastodonclient/view/fragment/TootListFragment.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/view/fragment/TootListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -97,6 +98,10 @@ class TootListFragment: Fragment(R.layout.fragment_toot_list) {
             tootList.clear()
             loadNext()
         }
+
+        isLoading.observe(viewLifecycleOwner, Observer {
+            binding?.swipeRefreshLayout?.isRefreshing = it
+        })
 
         loadNext()
     }
