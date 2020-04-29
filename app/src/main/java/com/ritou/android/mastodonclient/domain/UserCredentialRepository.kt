3d81da61d7fbf939ce710c179a3ee149579bcd18
R.Ito <kawboy442@gmail.com>
2020-04-29 17:04:50 +0900

    feat: UserCredentialRepositoryの内容を記述

--- app/src/main/java/com/ritou/android/mastodonclient/domain/UserCredentialRepository.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/domain/UserCredentialRepository.kt
@@ -1,4 +1,22 @@
 package com.ritou.android.mastodonclient.domain
 
-class UserCredentialRepository {
+import android.app.Application
+import com.ritou.android.mastodonclient.BuildConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class UserCredentialRepository(
+    private val application: Application
+) {
+    suspend fun find(
+        instanceUrl: String,
+        username: String
+    ): UserCredential? = withContext(Dispatchers.IO) {
+        
+        return@withContext UserCredential(
+            BuildConfig.INSTANCE_URL,
+            BuildConfig.USERNAME,
+            BuildConfig.ACCESS_TOKEN
+        )
+    }
 }
