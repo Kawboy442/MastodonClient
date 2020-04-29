943684e25e8cdea2d8c5647e33c772755112215c
R.Ito <kawboy442@gmail.com>
2020-04-29 16:52:17 +0900

    feat: ホームタイムラインを取得するAPIリクエストを設定

--- app/src/main/java/com/ritou/android/mastodonclient/data/MastodonApi.kt
+++ app/src/main/java/com/ritou/android/mastodonclient/data/MastodonApi.kt
@@ -2,6 +2,7 @@ package com.ritou.android.mastodonclient.data
 
 import com.ritou.android.mastodonclient.domain.Toot
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface MastodonApi {
@@ -11,4 +12,10 @@ interface MastodonApi {
          @Query("max_id") maxId: String? = null,
          @Query("only_media") onlyMedia: Boolean = false
     ): List<Toot>
+
+    @GET ("api/v1/timelines/home")
+    suspend fun feachHomeTimeline(
+        @Header("Authorization") accessToken: String,
+        @Query("max_Id") maxId: String? = null
+    )
 }
